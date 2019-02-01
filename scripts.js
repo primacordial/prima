e3335610303d5adc4f1ccfe14764e943155f8d5e
SarahCobham97 <47251146+SarahCobham97@users.noreply.github.com>
2019-02-01 17:40:13 +0000

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -43,7 +43,7 @@ var authTag = {
 	moderatorTag: "<span style='color: #00048E'><b>[Moderator]<timestamp/>",
 	adminTag: "<span style='color: #8E0000'><b>[Administrator]<timestamp/>",
 	ownerTag: "<span style='color: #210433'><b>[Owner]<timestamp/>",
-	hiddenTag: authTag.userTag
+	hiddenTag: "<span style='color: #465060'><b>[Member]<timestamp/>"
 	
 	// Custom Tags Here
 };
